b1a181b5b83b6613458c6166ae7d8e83597d8851
Pedro Saraiva <pedro.a34195@gmail.com>
2018-10-23 14:08:14 +0100

    test deploy

--- src/server/index.js
+++ src/server/index.js
@@ -52,7 +52,8 @@ app.get('/auth/google/callback',
     }
 )
 
-server.listen(3001, () => {
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
     console.log('Server is running on port 3001')
 })
 
