608073e49f124eb9b69c359281b0871fcdac442c
Pedro Saraiva <pedro.a34195@gmail.com>
2018-10-24 13:56:03 +0100

    glow indicator wip

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { Spring } from 'react-spring'
-
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 
+import { Transition, animated, Keyframes, config } from 'react-spring'
+import delay from 'delay'
+
 import Card from './Card'
+
+const Container = Keyframes.Spring({
+    // Single props
+    show: { from: { boxShadow: 0 }, to: { boxShadow: 20 }, config: config.default},
+    // Chained animations (arrays)
+    showAndHide: [ { to: { opacity: 1 } }, { to: { opacity: 0 } }],
+    // Functions with side-effects with access to component props
+    glow: async (next, ownProps) => {
+        await next({ from: { boxShadow: 0 }, to: { boxShadow: 25 }, config: config.default })
+            console.log('im here2')
+        //await delay(100)
+        await next({ from: { boxShadow: 25 }, to: { boxShadow: 0 }, config: config.default })
+            console.log('im here3')
+    }
+})
 
 class Hand extends React.Component {
 
     state = {
         spacing: '0',
+        reset: false
     }
 
     buildHand = () => {
         const { cards } = this.props
         return cards.map(c =>
-            <Grid key={c} item>
-                <Card id={c} />
-            </Grid>
+        <Grid key={c} item>
+            <Card id={c} />
+        </Grid>
         )
+    }
+
+    reset = (other, a, b, c, d) => {
+        //setInterval(() => {
+        if (other.boxShadow === 0) {
+            this.setState({reset: true})
+            console.log(other, a, b, c, d)
+        }
     }
 
     render() {
@@ -29,25 +54,37 @@ class Hand extends React.Component {
 
         const hand = this.buildHand()
 
-        return (
-            <div>
-                <Grid item xs={12}>
-                    <Grid
-                        container
-                        justify="center"
-                        spacing={Number(spacing)}
-                    >
-                        <div className={canPlay ? classes.turnIndicatorOn : classes.turnIndicatorOff}>
-                            {hand}
-                        </div>
+        const glowHand = <Container reset={this.state.reset} onRest={this.reset} state="glow">
+            { styles => {
+                return <div className={classes.test} style={{boxShadow: `0 0 ${styles.boxShadow}px #ffcf40`}}>
+                    {hand}
+                </div>}
+            }
+        </Container>
+
+
+            return (
+                <div>
+                    <Grid item xs={12}>
+                        <Grid
+                            container
+                            justify="center"
+                            spacing={Number(spacing)}
+                        >
+                            {canPlay? glowHand : hand}
+                        </Grid>
                     </Grid>
-                </Grid>
-            </div>
-        )
+                </div>
+            )
     }
 }
 
 const styles = theme => ({
+    test: {
+        backgroundColor: 'transparent',
+        borderRadius: '20px',
+        display: 'inline-flex'
+    },
     root: {
         flexGrow: 1,
     },
@@ -64,16 +101,6 @@ const styles = theme => ({
         padding: '5px',
         display: 'flex'
     },
-    snake: {
-        width: '10px',
-        height: '10px',
-        borderRadius: '20px',
-        backgroundColor: 'red',
-        position: 'absolute',
-        bottom: '0px',
-        left: '0px'
-    }
-
 })
 
 const mapStateToProps = (state) => {
